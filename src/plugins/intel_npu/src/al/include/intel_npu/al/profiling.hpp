bf8f1324feee8062edb564f0b81c47c90482bd19
flex-kgan <kian.yong.gan@intel.com>
2024-08-21 17:51:47 +0800

    test

--- src/plugins/intel_npu/src/al/include/intel_npu/al/profiling.hpp
+++ src/plugins/intel_npu/src/al/include/intel_npu/al/profiling.hpp
@@ -15,7 +15,7 @@ using LayerStatistics = std::vector<ov::ProfilingInfo>;
 template <typename LayerInfo>
 LayerStatistics convertLayersToIeProfilingInfo(const std::vector<LayerInfo>& layerInfo) {
     LayerStatistics perfCounts;
-
+    printf(" Debug - convertLayersToIeProfilingInfo() start 1 \n");
     perfCounts.reserve(layerInfo.size());
     for (const auto& layer : layerInfo) {
         ov::ProfilingInfo& info = perfCounts.emplace_back();
@@ -25,6 +25,7 @@ LayerStatistics convertLayersToIeProfilingInfo(const std::vector<LayerInfo>& lay
         const auto cpu_time_ns = std::chrono::nanoseconds(layer.dma_ns + layer.sw_ns + layer.dpu_ns);
         info.cpu_time = std::chrono::duration_cast<std::chrono::microseconds>(cpu_time_ns);
         info.node_name = layer.name;
+        printf(" Debug - convertLayersToIeProfilingInfo() running 1 \n");
         if (layer.sw_ns > 0) {
             info.exec_type = "Shave";
         } else if (layer.dpu_ns > 0) {
@@ -34,6 +35,7 @@ LayerStatistics convertLayersToIeProfilingInfo(const std::vector<LayerInfo>& lay
         }
         info.node_type = layer.layer_type;
     }
+    printf(" Debug - convertLayersToIeProfilingInfo() start 2 \n");
     return perfCounts;
 }
 
